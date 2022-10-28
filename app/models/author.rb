4e7144c32eae96cc1ec82207f17b1267c6fad7f2
Muthiira <calebmuchiri48@gmail.com>
2022-10-28 22:24:42 +0300

    author initialize

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,11 @@
 require 'pry'
 class Author 
+  # changed to reader because it cannot be changed after being initialized 
   attr_reader :name
   def initialize(name)
     @name = name
   end
-  
+
 end
 binding.pry
 # Article = super
